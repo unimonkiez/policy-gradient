964fe2992d8383959c94110928bf71635a6358c9
Yuval Saraf <unimonkiez@gmail.com>
2020-12-28 10:38:56 +0200

    Initial commit

---
+++ PGAgent.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
+class PolicyGradientAgent(object):
+    def __init__(self, env, lr=0.1):
+        self.num_actions = env.action_space.n
+        self.num_features = env.observation_space.shape[0]
+        self.W = np.zeros((self.num_features, self.num_actions))
+        self.b = np.zeros((self.num_actions))
+        self.lr = lr
+
+    def action_probability(self, state):
+        '''
+        Compute p(a|s) for discrete action using linear model on state
+        :param state: environment state
+        :return: vector of probabilities
+        '''
+        #TODO
+
+    def get_action(self, state):
+        '''
+        Selects a random action according to p(a|s)
+        :param state: environment state
+        :return: action
+        '''
+        probs = self.action_probability(state)
+        return np.random.choice(self.num_actions, p=probs)
+
+    def grad_log_prob(self, state, action):
+        '''
+        Compute gradient of log P(a|S) w.r.t W and b
+        :param state: environment state
+        :param action: descrete action taken
+        :return: dlogP(a|s)/dW, dlogP(a|s)/db
+        '''
+        # TODO
+
+    def update_weights(self, dW, db):
+        '''
+        Updates weights using simple gradient ascent
+        :param dW: gradients w.r.t W
+        :param db: gradients w.r.t b
+        '''
+        self.W += self.lr * dW
+        self.b += self.lr * db
+
+
---
+++ train.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PGAgent import PolicyGradientAgent
+import gym
+import argparse
+from datetime import datetime
+
+parser = argparse.ArgumentParser('')
+parser.add_argument('--lr',
+                    help='learning rate',
+                    type=float,
+                    default=.1)
+parser.add_argument('--N',
+                    help='Number of updates',
+                    type=int,
+                    default=2000)
+parser.add_argument('--b',
+                    help='batch size',
+                    type=int,
+                    default=1)
+parser.add_argument('--seed',
+                    help='random seed',
+                    type=int,
+                    default=1)
+parser.add_argument('--RTG',help='reward to go', dest='RTG', action='store_true')
+parser.add_argument('--baseline',help='use baseline', dest='baseline', action='store_true')
+
+
+
+args = parser.parse_args()
+np.random.seed(args.seed)
+
+env = gym.make('CartPole-v1')
+
+print('Enviourment: CartPole-v1 \nNumber of actions: ' ,env.action_space.n,'\nDimension of state space: ',np.prod(env.observation_space.shape))
+def run_episode(env, agent ,reward_to_go =False ,baseline=0.):
+    state = env.reset()
+    rewards = []
+    terminal = False
+    while not terminal:
+        action = agent.get_action(state)
+        state, reward, terminal, _ = env.step(action)
+        rewards.append(reward)
+        #TODO fill in
+    return dW, db, sum(rewards)
+
+
+def train(env, agent,args):
+    rewards = []
+    for i in range(args.N):
+        dW = np.zeros_like(agent.W)
+        db = np.zeros_like(agent.b)
+        for j in range(args.b):
+            #TODO fill in
+
+        if i%100 == 25:
+            temp = np.array(rewards[i - 25:i])
+            dateTimeObj = datetime.now()
+            timestampStr = dateTimeObj.strftime("%H:%M:%S")
+            print('{}: [{}-{}] reward {:.1f}{}{:.1f}'.format(timestampStr,i-25,i,np.mean(temp),u"\u00B1",np.std(temp)/np.sqrt(25)))
+    return agent, rewards
+
+def test(env, agent):
+    rewards = []
+    print('_________________________')
+    print('Running 500 test episodes....')
+    for i in range(500):
+        _,_,r,counter = run_episode(env,agent)
+        rewards.append(r)
+    rewards = np.array(rewards)
+    print('Test reward {:.1f}{}{:.1f}'.format(np.mean(rewards),u"\u00B1",np.std(rewards)/np.sqrt(500.)))
+    return agent, rewards
+
+
+agent = PolicyGradientAgent(env)
+agent, rewards = train(env,agent,args)
+print('Average training rewards: ',np.mean(np.array(rewards)))
+test(env,agent)
+plt.plot(np.cumsum(rewards))
+plt.show()
